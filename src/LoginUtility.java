683c1f6f76ac65ddcb7568e41b7e8268740011fd
Yusef-31 <74149510+Yusef-31@users.noreply.github.com>
2020-11-10 16:28:09 -0500

    Added student text.

--- src/LoginUtility.java
+++ src/LoginUtility.java
@@ -1,5 +1,6 @@
 public class LoginUtility {
     public static void main(String[] args) {
         System.out.println(" This is log in utility site.");
+        System.out.println(" Works great for students.");
     }
 }
