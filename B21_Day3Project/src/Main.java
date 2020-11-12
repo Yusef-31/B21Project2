523ced2f08a563c4e1274f9ea7cc81517eb53fbe
Yusef-31 <74149510+Yusef-31@users.noreply.github.com>
2020-11-12 10:48:44 -0500

    Created 3 new Strings

--- B21_Day3Project/src/Main.java
+++ B21_Day3Project/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(" Hello Git Last Day");
+        System.out.println(" B21 is excited !!!");
+        System.out.println(" B21 is dedicated");
+        System.out.println(" B21 are successful testers");
     }
 }
